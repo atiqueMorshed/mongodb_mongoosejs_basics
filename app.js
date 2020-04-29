c0d81afb52fcf39a217cca630322cdde4a0ca7c9
ArrowX17 <iamsamix17@gmail.com>
2020-04-29 20:13:06 +0600

    Relations

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+//------------------------------------------------------------
+// Closing Connection
+//------------------------------------------------------------
+
+// mongoose.connection.close();
 
 //------------------------------------------------------------
 // Insert
@@ -56,6 +61,14 @@ const banana = new Fruit({
   review: "Bissiri"
 });
 
+const pineapple = new Fruit({
+  name: "Pineapple",
+  rating: 7,
+  review: "Meh"
+});
+
+// pineapple.save();
+
 // Using Model to bulk save Documents to Collection.
 // Fruit.insertMany([kiwi, orange, banana], function(err) {
 //   if(err)
@@ -66,11 +79,14 @@ const banana = new Fruit({
 
 ///////////////////////////////////////////////////////////////
 
-// People
+//  Creating relationships between collections
+
+//People
 //Schema
 const peopleSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favoriteFruit: fruitSchema // Creating RELATIONSHIPS between collections
 });
 
 //Model
@@ -78,8 +94,9 @@ const People = mongoose.model("People", peopleSchema);
 
 //Document
 const people = new People({
-  name: "John",
-  age: 69
+  name: "Amy",
+  age: 69,
+  favoriteFruit: pineapple // Relation between document Amy and document pineapple. 
 });
 
 //Add Document to Collection
@@ -96,21 +113,23 @@ const people = new People({
 //------------------------------------------------------------
 
 // Using Model to find Documents from Collection
-Fruit.find(function(err, fruits) { // here err means the error msg AND fruits means if there are no error found, the results that we get back are kept in fruits var. | can be any variable, here we are calling fruits cause we are getting back fruits, its not related to the collections name :3 
-  mongoose.connection.close();
-  if(err)
-    console.log(err);
-  else{
-    fruits.forEach(function(fruit) {
-      console.log(fruit.name);
-    });
-  }
-});
+
+// Fruit.find(function(err, fruits) { // here err means the error msg AND fruits means if there are no error found, the results that we get back are kept in fruits var. | can be any variable, here we are calling fruits cause we are getting back fruits, its not related to the collections name :3 
+//   mongoose.connection.close();
+//   if(err)
+//     console.log(err);
+//   else{
+//     fruits.forEach(function(fruit) {
+//       console.log(fruit.name);
+//     });
+//   }
+// });
 
 //------------------------------------------------------------
 // Update
 //------------------------------------------------------------
 
+// Fruit
 // Fruit is the model, we want to update the 3rd document to set a name field. 
 // Fruit.updateOne({_id: "5ea9540012e8182ad421d251"}, {name: "Strawberry"}, function(err) { // model.updateOne({what document to target}, {update what you want}, callbackFunction(err) {});
 //   if(err)
@@ -119,18 +138,36 @@ Fruit.find(function(err, fruits) { // here err means the error msg AND fruits me
 //     console.log("Successfully updated name.");
 // });
 
+
+// People
+// People.updateOne({name: "John"}, {favoriteFruit: orange}, function(err) {
+//   if(err)
+//     console.log(err);
+//   else
+//     console.log("Successfully added favoriteFruit of Orange to John");
+// });
+
 //------------------------------------------------------------
 // Delete
 //------------------------------------------------------------
 
-Fruit.deleteOne({_id: "5ea9540012e8182ad421d251"}, function(err) {
-  if(err)
-    console.log("err", err);
-  else
-    console.log("Successfully deleted document/ row.");
-});
+// Fruit.deleteOne({_id: "5ea985010c9e0505c4a574f0"}, function(err) {
+//   if(err)
+//     console.log("err", err);
+//   else
+//     console.log("Successfully deleted one document/ row.");
+// });
 
 // Deleting a key/ Column from document/ Row.
 // $unset
 
 // User.update({_id: user._id}, {$unset: {field: 1 }}, callback);
+
+// Deleting many Documents/ Rows at once
+
+// People.deleteMany({name: "John"}, function(err) {
+//   if(err)
+//     console.log("err", err);
+//   else
+//     console.log("Successfully deleted many Documents/ Rows from Collection/ table.");
+// });
